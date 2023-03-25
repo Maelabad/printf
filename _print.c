1a7948cab464083456b80baa481c99d1851cd99e
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-03-25 22:08:44 +0000

    _printf

--- _print.c
+++ _print.c
@@ -11,34 +11,36 @@ int _printf(const char * const format, ...)
 	va_list args;
 	int i = 0, j = 0, val = 0;
 	Print funcs[] = {
-		{"c", print_char},
-		{"s", print_string},
+		{"c", print_char}, {"s", print_string},
 		/*As the formats d and i both refer to int*/
-		{"i", print_int},
-		{"d", print_int}
-
+		{"i", print_int}, {"d", print_int}, {"b", int_to_binary}
 	};
 
 	va_start(args, format);
 	while (format && (*(format + i)))
 	{
-		j = 0;
+		j = 0;	
 		if ((*(format + i) == '%'))
 		{
 			while (j < 4 && (*(format + i + 1) != *(funcs[j].format)))
 				j++;
-		}
-
-		if (j < 4 && j != 0)
-		{
-			val += funcs[j].p(args);
-			i++;
+			if (j < 5)
+			{	
+				val += funcs[j].p(args);
+				i++;
+			}
+			else
+			{
+				_putchar(*(format + i));
+				val++;
+			}
 		}
 		else
 		{
 			_putchar(*(format + i));
 			val++;
 		}
+
 		i++;
 
 	}

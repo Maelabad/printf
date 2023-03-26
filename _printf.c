91cfe60d261f7778e019d882a77d95384a1c9807
Maelabad <120084291+Maelabad@users.noreply.github.com>
2023-03-26 01:14:28 +0000

    _printf

--- _printf.c
+++ _printf.c
@@ -12,20 +12,20 @@ int _printf(const char * const format, ...)
 	int i = 0, j = 0, val = 0;
 	Print funcs[] = {
 		{"c", print_char}, {"s", print_string}, {"i", print_int}
-		,{"d", print_int}, {"b", int_to_binary}, {"u", print_format_u},
+		, {"d", print_int}, {"b", int_to_binary}, {"u", print_format_u},
 		{"o", print_format_o}, {"x", int_to_hexa_x}, {"X", int_to_hexa_X}
 	};
 
 	va_start(args, format);
 	while (format && (*(format + i)))
 	{
-		j = 0;	
+		j = 0;
 		if ((*(format + i) == '%'))
 		{
 			while (j < 9 && (*(format + i + 1) != *(funcs[j].format)))
 				j++;
 			if (j < 9)
-			{	
+			{
 				val += funcs[j].p(args);
 				i++;
 			}

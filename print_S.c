380317ba5af2ef579f15465875efd2259c59fc57
Maelabad <adjoboelie@gmail.com>
2023-03-26 04:43:45 -0700

    String

--- print_S.c
+++ print_S.c
@@ -6,33 +6,33 @@
 */
 int print_S(va_list arg)
 {
-        int count = 0, i;
+	int count = 0, i;
 
-        char *str = va_arg(arg, char *);
-        if (str == NULL)
-                str = "(null)";
+	char *str = va_arg(arg, char *);
 
-        for (i = 0; str[i]; i++)
-        {
-                if (str[i] < 32 || str[i] >= 127)
-                {  
-                        _putchar('\\');
-                        _putchar('x');
-                        count += 2;
-                        if (str[i] < 17)
-                        {
-                                _putchar('0');
-                                count++;
-                        }
-                        count += print_hexa(str[i],55);
-                }
-                else
-                {
-                        _putchar(str[i]);
-                        count++;
-                }
-        }
-        return (count);
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			if (str[i] < 17)
+			{
+				_putchar('0');
+				count++;
+			}
+			count += print_hexa(str[i], 55);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
 }
-
 
